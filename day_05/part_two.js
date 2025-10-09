cf329f4996b937b7612bc8ad0254641154cc2966
ronald-luo <r.luo.180@gmail.com>
2025-10-09 19:02:54 -0400

    part two day 05

--- day_05/part_two.js
+++ day_05/part_two.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-let lines = fs.readFileSync("short.txt", "utf8");
+let lines = fs.readFileSync("long.txt", "utf8");
 let linesArray = lines.split("\n");
 
 let ruleDict = {};
@@ -29,10 +29,7 @@ for (let line of linesArray) {
 // console.log(ruleDict);
 // console.log(pages);
 
-// if the first page is not in ruleDict, then it cant be valid
-// the page following the first page should exist in the array, if it doesn't then it cannot be valid.
-let validPages = [];
-// get valid pages
+let invalidPages = [];
 for (let page of pages) {
   let valid = true;
   for (let i = 1; i < page.length; i++) {
@@ -40,14 +37,46 @@ for (let page of pages) {
       valid = false;
     }
   }
-  if (valid) {
-    validPages.push(page);
+  if (!valid) {
+    invalidPages.push(page);
   }
 }
 
-// get middle page number and sum them
-let sum = 0;
-for (page of validPages) {
-  sum += Number(page[Math.floor(page.length / 2)]);
+// topological sort DFS
+let answerArr = [];
+
+for (let page of invalidPages) {
+  const updateSet = new Set(page);
+  const visited = {};
+  const order = [];
+
+  let helper = (node) => {
+    if (node in visited && visited[node].permanent) return;
+    if (node in visited && visited[node].temporary) return;
+
+    visited[node] = {};
+    visited[node].temporary = true;
+    visited[node].permanent = false;
+
+    let edges = ruleDict[node] || [];
+
+    for (let edge of edges) {
+      if (updateSet.has(edge)) helper(edge);
+    }
+
+    visited[node].permanent = true;
+    order.push(node);
+  };
+
+  for (const node of page) if (!visited[node]) helper(node);
+  const ordered = order.reverse();
+  answerArr.push(ordered);
 }
+
+let sum = 0;
+
+for (let arr of answerArr) {
+  sum += Number(arr[Math.floor(arr.length / 2)]);
+}
+
 console.log(sum);
