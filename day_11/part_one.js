5c24b387aaafa926e06e2beeb8a314a20d8d91f8
ronald-luo <r.luo.180@gmail.com>
2025-10-17 22:45:42 -0400

    clean up day 11 pt 1

--- day_11/part_one.js
+++ day_11/part_one.js
@@ -21,31 +21,10 @@ while (count > 0) {
     } else if (val.length % 2 === 0) {
       // number is even
 
-      // "10" -> "1" "0"
-      // "1423400243" -> "14234" + "00243" -> "243"
       let first = val.slice(0, val.length / 2);
       let second = val.slice(val.length / 2, val.length);
 
-      // if (second[0] === "0" && second.length !== 1) {
-      //   // "00243" cut off leading zeros
-      //   // "002405"
-      //   let prev = second[0];
-      //   let cut = 0;
-      //   for (let i = 1; i < second.length; i++) {
-      //     if (second[i] === prev) {
-      //       continue;
-      //     } else {
-      //       cut = i;
-      //       break;
-      //     }
-      //   }
-
-      //   second = second.slice(cut, second.length);
-      // }
       if (second[0] === "0" && second.length !== 1) {
-        // second = second.replace(/^0+(?!$)/, "");
-
-        // first = Number(first)
         second = Number(second);
         second = String(second);
       }
@@ -55,13 +34,11 @@ while (count > 0) {
     } else {
       // number is odd, so multiple by 2024
       newResult.push(String(Number(val) * 2024));
-      // newResult.push((BigInt(val) * 2024n).toString());
     }
   }
 
   result = newResult;
-  // console.log(result);
-  // if (count === 20) break;
+
   count--;
 }
 
