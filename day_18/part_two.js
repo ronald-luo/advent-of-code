ca633c68f92a66008d2e215f8e9ea6476bb5c104
ronald-luo <r.luo.180@gmail.com>
2025-10-24 19:56:29 -0400

    finish day 18 part 2

---
+++ day_18/part_two.js
@@ -0,0 +1,81 @@
+const fs = require("fs");
+
+// pre-process input into matrix and wall set
+
+let lines = fs.readFileSync("large.txt", "utf8");
+let linesArray = lines.split("\n");
+
+let corrupted = linesArray.map((node) => {
+  let [colWall, rowWall] = node
+    .trim()
+    .split(",")
+    .map((str) => Number(str));
+
+  return [colWall, rowWall];
+});
+
+corrupted = corrupted.slice(0, 2920);
+
+let walls = new Set();
+
+for (let wall of corrupted) {
+  let [colWall, rowWall] = wall;
+  walls.add(`${colWall},${rowWall}`);
+}
+
+// set up costs dict
+
+let costs = {};
+let rows = 71;
+let cols = 71;
+
+for (let r = 0; r < rows; r++) {
+  for (let c = 0; c < cols; c++) {
+    costs[`${c},${r}`] = Infinity;
+  }
+}
+
+costs["0,0"] = 0;
+
+// djikstras algorithm
+
+let queue = [[0, 0]];
+let directions = [
+  [1, 0],
+  [-1, 0],
+  [0, 1],
+  [0, -1],
+];
+
+while (queue.length > 0) {
+  let [currCol, currRow] = queue.shift();
+
+  for (let direc of directions) {
+    let [direcCol, direcRow] = direc;
+    let [nextCol, nextRow] = [currCol + direcCol, currRow + direcRow];
+
+    let rowBound = 0 <= nextRow && nextRow < rows;
+    let colBound = 0 <= nextCol && nextCol < cols;
+
+    if (rowBound && colBound) {
+      if (!walls.has(`${nextCol},${nextRow}`)) {
+        let nextCost = costs[`${nextCol},${nextRow}`];
+        let betterCost = costs[`${currCol},${currRow}`] + 1;
+
+        // only add it to the queue if its better
+        if (costs[`${currCol},${currRow}`] + 1 < nextCost) {
+          costs[`${nextCol},${nextRow}`] = Math.min(nextCost, betterCost);
+          queue.push([nextCol, nextRow]);
+        }
+      }
+    }
+  }
+}
+
+// manually binary search through potential answers
+// find last wall added when cost of 70,70 becomes Infinity (not visitable)
+// see line 17, where we i manually check numbers between 0 and 3450 (length of input)
+// 2000 -> 3450 -> 2500 -> 2900 -> 2950 -> 2925 -> 2915 -> 2918 -> 2920
+console.log(costs);
+console.log(corrupted.at(-1));
+console.log(linesArray.length);
