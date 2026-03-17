ff115bdaba7e83c5cefecbe25b340984467838bf
ronald-luo <r.luo.180@gmail.com>
2026-03-17 03:13:28 -0400

    finish year 2020 day_03 pt 2

---
+++ 2020/day_03/part_two.js
@@ -0,0 +1,47 @@
+let fs = require("fs");
+
+let lines = fs.readFileSync("large.txt", "utf-8");
+
+lines = lines.split("\n").map((line) => line.trim());
+
+let hill = lines.map((node) => {
+  return node.split("");
+});
+// console.log(hill);
+
+let rows = hill.length;
+let cols = hill[0].length;
+// console.log(rows, cols);
+
+let slopes = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2],
+];
+
+for (let [run, rise] of slopes) {
+  let queue = [[0, 0]];
+  let treeCount = 0;
+
+  while (queue.length > 0) {
+    let [currCol, currRow] = queue.shift();
+
+    let colBound = 0 <= currCol && currCol < cols;
+    let rowBound = 0 <= currRow && currRow < rows;
+
+    if (!rowBound) break;
+
+    if (hill[currRow][currCol] === "#") treeCount++;
+    // console.log(hill[currRow][currCol], currCol, currRow);
+
+    let [nextCol, nextRow] = [currCol + run, currRow + rise];
+    if (nextCol >= cols) nextCol = nextCol % cols;
+    queue.push([nextCol, nextRow]);
+  }
+
+  console.log(treeCount); // 61 * 257 * 64 * 47 * 37
+}
+
+console.log(61 * 257 * 64 * 47 * 37);
