3320aa523469825ef7e749415845d4f0b3b5ba4f
ronald-luo <r.luo.180@gmail.com>
2026-06-17 21:57:12 -0400

    finish day_04 year 2019

---
+++ 2019/day_04/part_two.js
@@ -0,0 +1,39 @@
+let fs = require("fs");
+
+let lines = fs.readFileSync("small.txt", "utf-8");
+
+// Figure out possible passwords from facts
+// Time: O(N * M), Space: O(1), Output: Number (of possibilities)
+// Preconditions: six digit number, not decreasing, two adjacent values are the same
+
+let [lo, hi] = lines.split("-").map(Number);
+
+let possible = 0;
+
+for (lo; lo <= hi; lo++) {
+  let number = `${lo}`;
+  let hasNoDecreasing = true;
+  let hasTwoAdjacent = false;
+
+  // console.log(lo);
+
+  for (let i = 1; i < number.length; i++) {
+    // console.log(number[i - 1], number[i]);
+
+    if (number[i] === number[i - 1]) {
+      if (i < number.length && number[i] === number[i + 1]) {
+        continue;
+      } else if (i > 1 && number[i - 2] === number[i - 1]) {
+        continue;
+      }
+      hasTwoAdjacent = true;
+    } else if (number[i] < number[i - 1]) {
+      hasNoDecreasing = false;
+      break;
+    }
+  }
+
+  if (hasNoDecreasing && hasTwoAdjacent) possible++;
+}
+
+console.log("possible are", possible);
